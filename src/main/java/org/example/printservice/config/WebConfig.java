78b38f36ec0a673e3a9e221941a50f9b753d249d
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-05-24 09:22:25 +0300

    changed cors origin pattern

--- src/main/java/org/example/printservice/config/WebConfig.java
+++ src/main/java/org/example/printservice/config/WebConfig.java
@@ -6,11 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig {
-
-//    @Value("${frontend-url}")
-//    private String frontendUrl;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -19,7 +18,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
+                        .allowedOriginPatterns("https://34.116.235.108")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
